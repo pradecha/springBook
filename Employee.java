9c72ab17576903d58e63b2d21f7472a4dc956789
pradecha <pradeepchaudhary358@.com>
2022-10-18 11:35:11 +0530

    its updated

---
+++ Employee.java
@@ -0,0 +1,65 @@
+
+
+package com.identify;
+
+// POJO or Model or Bean
+public class Employee {
+	
+	//Attribute 
+	int Id;
+	String empName;
+	int empSalary;
+	
+	// Constructor 
+	public Employee() {
+		
+	}
+	
+	public Employee(int id, String empName, int empSalary) {
+		super();
+		Id = id;
+		this.empName = empName;
+		this.empSalary = empSalary;
+	}
+	
+	// Getter and Setter method
+	
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public int getEmpSalary() {
+		return empSalary;
+	}
+
+	public void setEmpSalary(int empSalary) {
+		this.empSalary = empSalary;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [Id=" + Id + ", empName=" + empName + ", empSalary=" + empSalary + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
