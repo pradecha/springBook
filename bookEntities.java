0651c9cb22bdff449841adf5dfdb6d91df0f321c
pradecha <pradeepchaudhary358@.com>
2022-10-18 12:58:19 +0530

    Book Entities updated now

---
+++ bookEntities.java
@@ -0,0 +1,38 @@
+public class bookEntities {
+
+    int boodId;
+    String bookName;
+    String bookDescriptions;
+    
+    public int getBoodId() {
+        return boodId;
+    }
+    public void setBoodId(int boodId) {
+        this.boodId = boodId;
+    }
+    public String getBookName() {
+        return bookName;
+    }
+    public void setBookName(String bookName) {
+        this.bookName = bookName;
+    }
+    public String getBookDescriptions() {
+        return bookDescriptions;
+    }
+    public void setBookDescriptions(String bookDescriptions) {
+        this.bookDescriptions = bookDescriptions;
+    }
+    public bookEntities(int boodId, String bookName, String bookDescriptions) {
+        this.boodId = boodId;
+        this.bookName = bookName;
+        this.bookDescriptions = bookDescriptions;
+    }
+    @Override
+    public String toString() {
+        return "bookEntities [boodId=" + boodId + ", bookName=" + bookName + ", bookDescriptions=" + bookDescriptions
+                + "]";
+    }
+
+    
+    
+}
